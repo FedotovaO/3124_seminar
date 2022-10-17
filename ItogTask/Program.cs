83560918bf0dce187d4ee61ff357f90f005e42fd
Olga <chibiska@gmail.com>
2022-10-17 13:35:49 +0500

    readme

--- ItogTask/Program.cs
+++ ItogTask/Program.cs
@@ -2,3 +2,27 @@
 // длинна которых меньше либо равна 3 символа. 
 // Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
 
+string[] array1 = new string[5] {"start", "hot", "no", "hello", "2"};
+string[] array2 = new string[array1.Length];
+void ArraySortString(string[] array1, string[] array2) //метод сортировки строчек массива в новый массив
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array) //метод вывода отсортированного массива на экран
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+ArraySortString(array1, array2);
+PrintArray(array2);
